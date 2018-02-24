e90c5a5f231cd1445e9cdba9afe22c6755c430d6
Ellen Liu <ellen.m.liu@gmail.com>
2018-02-23 22:30:02 -0800

    Feat: Add basic Navbar
    Imported Bootstrap Navbar and set up with basic links. Links do not
    direct to anything at the moment.

--- static/components/header.js
+++ static/components/header.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap"
+import '../css/header.css';
 
 export default class App extends React.Component {
   render() {
-    return <h1>Cook That</h1>;
+    return <Navbar collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          Cook That
+        </Navbar.Brand>
+
+      </Navbar.Header>
+
+      <Navbar.Collapse>
+
+    <Nav pullRight>
+      <NavItem eventKey={1} href="#">
+        About
+      </NavItem>
+      <NavItem eventKey={2} href="#">
+        Login
+      </NavItem>
+    </Nav>
+  </Navbar.Collapse>
+
+    </Navbar>;
   }
 }
