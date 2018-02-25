a674ca71655500e8b378d754244228577b2fd38e
Ellen Liu <ellen.m.liu@gmail.com>
2018-02-24 22:42:30 -0800

    Feat: Add animation with table and a pot of stew

--- static/components/app.js
+++ static/components/app.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import '../css/home.css';
 
 export default class App extends React.Component {
   render() {
-    return <p>You did it!!</p>;
+    return <section>
+      <div id='pot'>
+        <div id='soup'>
+          <div id='spoon'></div>
+        </div>
+      </div>
+      <div id='table'></div>
+    </section>;
   }
 }
