e67587f7fac5b24ebdd90a7b068c552cef6dd8fd
Ellen Liu <ellen.m.liu@gmail.com>
2018-03-09 00:10:04 -0800

    Fix: Fix error cause by misplaced semi colon

--- static/components/header.js
+++ static/components/header.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-;dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
 import '../css/header.css';
 
