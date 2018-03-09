5fc9805ec562581c52db043fcfc0c71fa0d783a4
Ellen Liu <ellen.m.liu@gmail.com>
2018-03-09 11:18:32 -0800

    Style: Add main CSS to Login page

--- static/components/login.js
+++ static/components/login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './header';
+import '../css/index.css';
 
 export default class Login extends React.Component {
   render() {
