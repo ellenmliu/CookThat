1bab07ccb0c69791683937f944e52b2a9d17246b
Ellen Liu <ellen.m.liu@gmail.com>
2018-03-10 00:40:37 -0800

    Feat: Add input area to add in ingredients
    This will allow users to input their ingredients. Will work on getting
    it to save to the database later. Also think about putting in
    measurements so it could accurately find recipes.

--- static/components/dashboard.js
+++ static/components/dashboard.js
@@ -4,10 +4,42 @@ import Header from './header';
 import '../css/index.css';
 
 export default class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {ingredients:['apple','orange'], addIngredient:''}
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({addIngredient: event.target.value});
+  }
+
+  handleSubmit(event) {
+    let temp = this.state.ingredients;
+    temp.push(this.state.addIngredient);
+    this.setState({ingredients: temp});
+    event.preventDefault();
+  }
+
   render() {
     return (<main>
       <Header />
       <h1>Dashboard</h1>
+      <h2>Ingredients</h2>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Ingredient:
+          <input type="text" onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <ul>
+      {this.state.ingredients.map((ingredient) => {
+        return <li>{ingredient}</li>
+      })}
+    </ul>
     </main>);
   }
 }
